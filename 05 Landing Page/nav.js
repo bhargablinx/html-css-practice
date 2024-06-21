1534d224a03d76476a7c21d2ed501168193775f8
bhargabLinx <bhargabbhuyan001@gmail.com>
2024-06-21 11:52:57 +0530

    Links added for both desktop and mobile nav bar

--- 05 Landing Page/nav.js
+++ 05 Landing Page/nav.js
@@ -1,12 +1,12 @@
 const navBtn = document.querySelector(".nav-bar");
 const mobileNav = document.querySelector(".mobile-nav");
 const body = document.querySelector("body");
+const mobileMenu = document.querySelectorAll(".m-menu");
 
 let isNavOpen = false;
 
 navBtn.addEventListener('click', () => {
     isNavOpen = (isNavOpen === true)? false: true; // changes isNavOpen to its opposite value
-    
     if (isNavOpen) {
         mobileNav.style.display = 'block';
         body.style.overflowY = 'hidden'
@@ -15,3 +15,11 @@ navBtn.addEventListener('click', () => {
         body.style.overflowY = 'auto'
     }
 });
+
+mobileMenu.forEach((item) => {
+    item.addEventListener('click', () => {
+        mobileNav.style.display = 'none';
+        body.style.overflowY = 'auto'
+        isNavOpen = false;
+    })
+})
